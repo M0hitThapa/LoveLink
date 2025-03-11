e7a7b6b564f7a03ef937c0272b2c1799d386bba7
Mohit Thapa <thapamohit2058@gmail.com>
2025-03-11 22:28:13 +0545

    add little enhancement on navbar

--- src/components/navbar/TopNav.tsx
+++ src/components/navbar/TopNav.tsx
@@ -9,7 +9,7 @@ function TopNav() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className="sticky top-0 z-50 max-w-screen bg-rose-50 shadow rounded-md lg:mt-2 lg:mx-20 md:mt-2 md:mx-7 ">
+    <header className="sticky top-0 z-50 max-w-screen bg-rose-50  shadow rounded-md lg:mt-2 lg:mx-20 md:mt-2 md:mx-7 ">
       <nav className="flex items-center justify-between p-2 md:px-4">
         {/* Logo */}
         <h1 className="text-2xl font-black font-sans flex gap-2">
@@ -21,9 +21,9 @@ function TopNav() {
 
         {/* Desktop Navigation */}
         <div className="hidden md:flex gap-12">
-          <Link href="/members" className="text-rose-500 font-mono text-xl font-bold  hover:text-rose-600 hover:rotate-2 ">Matches</Link>
-          <Link href="/lists" className="text-rose-500 font-mono text-xl font-bold hover:text-rose-600 hover:rotate-2">Lists</Link>
-          <Link href="/messages" className="text-rose-500 text-xl font-bold font-mono hover:text-rose-600 hover:rotate-2">Messages</Link>
+          <Link href="/members" className="text-rose-500 font-mono text-xl font-bold h-8 px-2 rounded-sm   hover:text-rose-600 hover:rotate-2 hover:bg-rose-100 ">Matches</Link>
+          <Link href="/lists" className="text-rose-500 font-mono text-xl font-bold h-8 px-2 rounded-sm hover:text-rose-600 hover:rotate-2 hover:bg-rose-100">Lists</Link>
+          <Link href="/messages" className="text-rose-500 text-xl font-bold font-mono h-8 px-2 rounded-sm hover:text-rose-600 hover:rotate-2 hover:bg-rose-100">Messages</Link>
         </div>
 
         {/* Buttons (Always Visible) */}
@@ -40,7 +40,7 @@ function TopNav() {
 
         {/* Mobile Menu Button */}
         <button
-          className="md:hidden text-rose-800 "
+          className="md:hidden text-rose-800"
           onClick={() => setMenuOpen(!menuOpen)}
         >
           {menuOpen ? <X size={28} /> : <Menu size={28} />}
@@ -50,9 +50,9 @@ function TopNav() {
       {/* Mobile Menu */}
       {menuOpen && (
         <div className="md:hidden flex flex-col items-center gap-4 bg-rose-100  text-rose-500 font-bold  p-4 m-4 rounded-xl ">
-          <Link href="/members" className="text-rose-500 font-mono text-xl font-bold  hover:text-rose-600 hover:rotate-2 " onClick={() => setMenuOpen(false)}>Matches</Link>
-          <Link href="/lists" className="text-rose-500 font-mono text-xl font-bold  hover:text-rose-600 hover:rotate-2" onClick={() => setMenuOpen(false)}>Lists</Link>
-          <Link href="/messages" className="text-rose-500 font-mono text-xl font-bold  hover:text-rose-600 hover:rotate-2" onClick={() => setMenuOpen(false)}>Messages</Link>
+          <Link href="/members" className="text-rose-500 font-mono text-xl font-bold h-8 px-2 rounded-sm  hover:text-rose-600 hover:rotate-2 hover:bg-rose-100" onClick={() => setMenuOpen(false)}>Matches</Link>
+          <Link href="/lists" className="text-rose-500 font-mono text-xl font-bold h-8 px-2 rounded-sm hover:text-rose-600 hover:rotate-2 hover:bg-rose-100" onClick={() => setMenuOpen(false)}>Lists</Link>
+          <Link href="/messages" className="text-rose-500 font-mono text-xl font-bold h-8 px-2 rounded-sm   hover:text-rose-600 hover:rotate-2 hover:bg-rose-100" onClick={() => setMenuOpen(false)}>Messages</Link>
           <Button className="bg-rose-700 cursor-pointer hover:bg-rose-800 shadow-inner shadow-rose-600 transition duration-300 hover:rotate-3" onClick={() => setMenuOpen(false)}>
 
             <Link href="/login">Login</Link>
